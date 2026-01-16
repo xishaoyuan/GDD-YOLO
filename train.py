e46bde62c6476ae503ae3e61405ed8c16b0fed3e
xishaoyuan <94449911+xishaoyuan@users.noreply.github.com>
2026-01-16 18:34:32 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,20 +1,8 @@
 import warnings, os
-# os.environ["CUDA_VISIBLE_DEVICES"]="-1"    # 代表用cpu训练 不推荐！没意义！ 而且有些模块不能在cpu上跑
-# os.environ["CUDA_VISIBLE_DEVICES"]="0"     # 代表用第一张卡进行训练  0：第一张卡 1：第二张卡
-# 多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
+
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# BILIBILI UP 魔傀面具
-# 训练参数官方详解链接：https://docs.ultralytics.com/modes/train/#resuming-interrupted-trainings:~:text=a%20training%20run.-,Train%20Settings,-The%20training%20settings
-
-# 训练过程中loss出现nan，可以尝试关闭AMP，就是把下方amp=False的注释去掉。
-# 训练时候输出的AMP Check使用的YOLO11n的权重不是代表载入了预训练权重的意思，只是用于测试AMP，正常的不需要理会。
-
-# 使用项目前必看<项目视频百度云链接.txt>的第一行有一个必看的视频!!
-# 使用项目前必看<项目视频百度云链接.txt>的第一行有一个必看的视频!!
-# 使用项目前必看<项目视频百度云链接.txt>的第一行有一个必看的视频!!
-# 使用项目前必看<项目视频百度云链接.txt>的第一行有一个必看的视频!!
 
 if __name__ == '__main__':
     model = YOLO(r'E:\yolo\ultralytics-yolo11-20250301\ultralytics-20250301\yolo11-GlobalEdgeInformationTransfer.yaml')
